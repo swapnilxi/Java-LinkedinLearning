c2110687cf8be393e1e41b68bcf3461c562c429e
swapnil <swapnilg671@gmail.com>
2022-10-19 12:26:56 +0530

    personalised message

---
+++ functional Java/function.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class function {
+    static void sum(){
+       
+    Scanner sc = new Scanner(System.in);
+    System.out.println("enter number 1:");
+    int a=sc.nextInt();
+
+    System.out.println("enter the number 2: ");
+    int b= sc.nextInt();
+
+  
+    int sum = a+ b;
+    System.out.println("the sum is :"+ sum);
+}
+
+    
+public static void main(String[] args) {
+    System.out.println("this is functional programming");
+ sum();
+    
+}
+}
---
+++ functional Java/personalisedmes.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class personalisedmes {
+    public static void main(String[] args) {
+    Scanner sc= new Scanner(System.in);
+    System.out.println("enter your name: ");
+    String name= sc.nextLine();
+    String Greet = GreetingName(name);  
+    System.out.println(Greet);  
+    sc.close();
+    }
+
+    private static String GreetingName(String name) {
+        return greet()+name;
+    }
+
+    private static String greet() {
+        return "hello ";
+    }
+
+    
+}
