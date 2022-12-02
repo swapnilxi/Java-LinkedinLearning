4c346544369fb47536b559fe288a22f3cd8d903b
swapnil gupta <swapnilg671@gmail.com>
2022-12-02 19:18:08 +0530

    quick Sort

--- Java-Recursion/Sorting.java
+++ Java-Recursion/Sorting.java
@@ -6,8 +6,45 @@ public class Sorting {
     public static void main(String[] args) {
         int[] arr= {1,4,3,6,2,3};
         // System.out.println(Arrays.toString(mergeSort(arr))); 
-        mergeSortInPlace(arr, 0, arr.length);  
+        // mergeSortInPlace(arr, 0, arr.length);  
+        quickSort(arr,0, arr.length-1);
         System.out.println(Arrays.toString(arr));
+    }
+    static void quickSort( int[] arr, int lo, int  hi){
+        //java internally uses double pivot quick sort
+        //that has combination of heap sort and insertion sort
+        if(lo>hi){
+            return;
+        }
+        //for swapping
+        int start=lo;
+        int end=hi;
+        int mid= start+(end-start)/2;
+        int pivot= arr[mid];
+
+        if(start <=end){
+            while(arr[start]< pivot){
+                start++;
+            }
+            while (arr[end]> pivot) {
+                end--;
+            }
+            //swap till start is less than end
+            if (start<=end){
+                 int temp= arr[start];
+                 arr[start]=arr[end];
+                 arr[end]= temp;
+                 start++;
+                 end--;
+
+            }
+             
+        }
+        //now my pivot is at correct index, please sort left and right
+        quickSort(arr, lo, end);
+        quickSort(arr, start, hi);
+
+
     }
 
    
