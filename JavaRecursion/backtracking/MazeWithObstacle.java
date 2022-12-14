3b54247e5a2cf5d5f19d7d27d19d0bfde4584ab7
swapnil gupta <swapnilg671@gmail.com>
2022-12-14 14:11:40 +0530

    mazeobstacles

--- JavaRecursion/backtracking/MazeWithObstacle.java
+++ JavaRecursion/backtracking/MazeWithObstacle.java
@@ -1,20 +1,36 @@
 package backtracking;
-
+//find all possible path
 public class MazeWithObstacle {
     public static void main(String[] args) {
-        boolean[][] board={ true, true, true},
+        boolean[][] board={{ true, true, true},
         {true, false, true},
-        {true, true , true};
-possiblePath("", board, 0 , 0 );
+        {true, true , true}};
+possiblePath("", board, 0, 0);
     }
 
 static void possiblePath(String p, boolean[][] board, int i, int j) {
-    int n= board.length-1;
-    int m= board[0].length-1;
-
+    int m= board.length-1;
+    int n= board[0].length-1;
+   if(board[i][j]==false){
+   return;
     }
-
+    if(i==m&& j==n){
+        System.out.println(p);
+        return;
+     }
+    if(board[i][j]==false){
+        return; 
+    }
+   //down 
+   if(i<m){
+   possiblePath("D"+p, board, i+1,j);
+}
+   //right 
+   if(j<m){
+    possiblePath("R"+p, board, i,j+1);
+ }
    
+    }
     
 }
 
--- JavaRecursion/subset/subsetSum.java
+++ JavaRecursion/subset/subsetSum.java
@@ -1,7 +1,6 @@
 package subset;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
 
 public class subsetSum {
